86db8b7f6afd18d3144a11bbc6501cd68a3088be
Evstafiy Mikeilov <mikeilov.ea@gmail.com>
2025-08-17 23:55:55 +0300

    addendum периодических заданий

--- don/celery.py
+++ don/celery.py
@@ -6,4 +6,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'don.settings')
 app = Celery('don')
 app.config_from_object('django.conf:settings', namespace = 'CELERY')
 
+app.conf.beat_schedule = {
+    'print_every_5_seconds': {
+        'task': 'balon.tasks.printer',
+        'schedule': 5,
+        'args': (5,),
+    },
+}
+
 app.autodiscover_tasks()
