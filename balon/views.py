74a0e424617e3300090ff390feeeb94ae3ff564b
Evstafiy Mikeilov <mikeilov.ea@gmail.com>
2025-08-17 23:18:16 +0300

    add celery tasks

--- balon/views.py
+++ balon/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from .tasks import hello, printer
+from datetime import datetime, timedelta
 
-# Create your views here.
+class IndexView(View):
+    def get(self, request):
+        #  printer.delay(10)
+        #  printer.apply_async([10], countdown = 5)
+        printer.apply_async([10], eta = datetime.now() + timedelta(seconds = 5))
+        hello.delay()
+        return HttpResponse('Hello!')
